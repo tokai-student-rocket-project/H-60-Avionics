5a53a6c2dae5d21e7e10f94a58292aca611c9d7b
HirokiTsutsumi <hiroki1815@outlook.jp>
2024-12-04 02:45:04 +0900

    多分pio/buildはプッシュすべきでないが念のため...

---
+++ .pio/libdeps/SensingModule/Packetizer/examples/send_receive_manual/send_receive_manual.ino
@@ -0,0 +1,24 @@
+// #define PACKETIZER_SET_DEFAULT_ENCODING_SLIP
+// #define PACKETIZER_USE_CRC_AS_DEFAULT
+
+#include <Packetizer.h>
+
+auto decoder = Packetizer::getDecoderRef();
+
+void setup() {
+    Serial.begin(115200);
+    delay(2000);
+}
+
+void loop() {
+    while (const int size = Serial.available()) {
+        uint8_t data[size];
+        Serial.readBytes((char*)data, size);
+        decoder->feed(data, size);
+
+        while (decoder->available()) {
+            Packetizer::send(Serial, decoder->data(), decoder->size());
+            decoder->pop();
+        }
+    }
+}
